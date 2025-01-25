4bf929acd011c9c1f0501ca91898313a9cb4416c
Bob Kozdemba <bkozdemba@gmail.com>
2025-01-25 02:51:09 +0000

    cleaned up

--- 01-hello.py
+++ 01-hello.py
@@ -1,10 +1,9 @@
 import getpass
 import os
+from langchain_openai import ChatOpenAI
 
 if not os.environ.get("OPENAI_API_KEY"):
   os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
-
-from langchain_openai import ChatOpenAI
 
 model = ChatOpenAI(model="gpt-4o-mini")
 
